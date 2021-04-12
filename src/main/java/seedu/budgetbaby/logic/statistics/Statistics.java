6d3f5dfe89ad15ee6db958807f56b925a4add0d3
natosy <natosyy@gmail.com>
2021-04-12 13:54:23 +0800

    Fix Checkstyle

--- src/main/java/seedu/budgetbaby/logic/statistics/Statistics.java
+++ src/main/java/seedu/budgetbaby/logic/statistics/Statistics.java
@@ -1,6 +1,10 @@
 package seedu.budgetbaby.logic.statistics;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import javafx.collections.ObservableList;
